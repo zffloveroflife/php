1c7923020225bbf15efe67a811d0a1b68bd60c00
zff <zffloveroflife@163.com>
2014-07-04 22:52:47 +0800

    图片上传
    php 图片上传ok

--- uploadFile/one.php
+++ uploadFile/one.php
@@ -1,29 +1,16 @@
 <?php
 /* 文件上传
- *
  * 	1. 单个文件上传
- *
  * 	2. 多个文件上传
- * 
- * 
- * 
- *
- *	一、PHP配置文件中和上传文件有关的选项
- *  
+ *	一、PHP配置文件中和上传文件有关的选项 
  *     file_uploads = on 
- *
  *     upload_max_filesize= 200M  最大不要超过服务器的内存
- *
  *     upload_tmp_dir = c:/uploads/
- *
          *post_max_size = 250M
- *
  *     二、上传表单需要的注意事项
- *
  *    1. 如果有文件上传操作表单的提交方法必须 HTTP post 
  *    2. 表单上传需要使用type为file的表
- *    3. enctype="multipart/form-data" 只有文件上传时才使用这个值 ，用来指定表单编码的数据方式， 让服务器知道，我们要传递一个文件并带有常规的表单信息。
- *	
+ *    3. enctype="multipart/form-data" 只有文件上传时才使用这个值 ，用来指定表单编码的数据方式， 让服务器知道，我们要传递一个文件并带有常规的表单信息。	
  *	4. 建议添加一个 MAX_FILE_SIZE 隐藏表单， 值的单位也是字节
  *
  *
@@ -43,7 +30,7 @@
             [error] => 0
             [size] => 24485
     )
-
+上传多个文件是的参数
 Array
 (
     [pic] => Array
--- uploadFile/upload.php
+++ uploadFile/upload.php
@@ -1,6 +1,5 @@
 <?php
 	//step 1 使用$_FILES['pic']["error"] 检查错误
-
 	if($_FILES["pic"]["error"] > 0){
 		switch($_FILES["pic"]["error"]) {
 			case 1:
@@ -9,26 +8,19 @@
 			case 2:
 				echo "上传文件的大小超过了 HTML 表单中 MAX_FILE_SIZE 选项指定的值";
 				break;
-
 			case 3: 
 				echo "文件只有部分被上传";
 				break;
-
 			case 4:
 				echo "没有文件被上传";
 				break;
-
 			default:
-
 				echo "末知错误";
 		
-		}
-		
+		}		
 		exit;
 	}
-	
-
-	$maxsize=5000000;  //50k
+	$maxsize=5000000000;  //
 
 	//step 2 使用$_FILES["pic"]["size"] 限制大小 单位字节 2M=2000000
 	if($_FILES["pic"]["size"] > $maxsize ) {
@@ -40,7 +32,6 @@
 	//step 3 使用$_FILES["pic"]["type"]或是文件的扩展名 限制类型 MIME  image/gif  image/png    gif png jpg
 
 	/*   list($dl, $xl) = explode("/", $_FILES["pic"]["type"]);
-
 		if($dl!="image"){
 			echo "请上传一个图片，不充许其它类型文件";
 			exit;
@@ -48,28 +39,16 @@
 	 */
 	
 	$allowtype=array("png", "gif", "jpg", "jpeg");
-
-
 	$arr=explode(".", $_FILES["pic"]["name"]);
-
 	$hz=$arr[count($arr)-1];
-
 	if(!in_array($hz, $allowtype)){
 		echo "这是不充许的类型";
 		exit;
 	}
-	
-
-	
 	//step 4 将让传后的文件名改名
-
-	
 	$filepath="./uploads/";
-
 	$randname=date("Y").date("m").date("d").date("H").date("i").date("s").rand(100, 999).".".$hz;
-
 	//将临时位置的文件移动到指定的目录上即可
-
 	if(is_uploaded_file($_FILES["pic"]["tmp_name"])){
 		if(move_uploaded_file($_FILES["pic"]["tmp_name"], $filepath.$randname)){
 			echo "上传成功";
